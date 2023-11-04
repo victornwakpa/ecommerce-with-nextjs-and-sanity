83e9c49f6ac16502be8441ce63995f5ec5904b90
victornwakpa <nwakpavictor@gmail.com>
2023-11-04 16:43:28 +0100

    finished the hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,6 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
+import { client, urlFor } from "../lib/sanity";
 
-export default function Hero() {
+async function getData() {
+    const query = "*[_type == 'heroImage'][0]";
+
+    const data = await client.fetch(query);
+
+    return data;
+}
+
+export default async function Hero() {
+    const data = await getData()
     return (
         <section className="mx-auto max-w-2xl px-4 sm:pb-6 lg:max-w-7xl lg:px-8">
             <div className="mb-8 flex flex-wrap justify-between md:mb-36">
@@ -16,8 +27,39 @@ export default function Hero() {
 
                 <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
                     <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-200 shadow-lg md:left-16 md:top-16 lg:ml-0">
-                        <Image />
+                        <Image 
+                            src={urlFor(data.image1).url()} 
+                            alt="hero image"
+                            className="h-full w-full object-cover object-center "
+                            width={500}
+                            height={500}
+                        />
                     </div>
+
+                    <div className="overflow-hidden rounded-lg bg-gray-100 shadow-lg">
+                        <Image 
+                            src={urlFor(data.image2).url()}
+                            alt="Great photo"
+                            className="h-full w-full object-cover object-center"
+                            width={500}
+                            height={500}
+                            priority
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-between gap-8 md:flex-row">
+                <div className="flex h-12 w-64 divide-x overflow-hidden rounded-lg border">
+                    <Link href="/Men" className="flex w-1/3 items-center justify-center text-gray-500 transition durarion-100 hover:bg-gray-100 active:bg-gray-200">
+                        Men
+                    </Link>
+                    <Link href="/Women" className="flex w-1/3 items-center justify-center text-gray-500 transition durarion-100 hover:bg-gray-100 active:bg-gray-200">
+                        Women
+                    </Link>
+                    <Link href="/Teens" className="flex w-1/3 items-center justify-center text-gray-500 transition durarion-100 hover:bg-gray-100 active:bg-gray-200">
+                        Teens
+                    </Link>
                 </div>
             </div>
         </section>
